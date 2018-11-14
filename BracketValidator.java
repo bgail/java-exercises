30b7328a14ed761f12bb86ad5c37cf6e8b2ce841
bgail <mabigail1123@gmail.com>
2018-11-14 00:09:44 -0700

    needs improvement

---
+++ BracketValidator.java
@@ -0,0 +1,37 @@
+package ex;
+import java.util.*;
+
+public class BracketValidator {
+
+	public static void main(String[] args) {
+		System.out.println("{ [ ] ( ) } should return true:" + validateBracket("{[]()}"));
+		System.out.println("{ [ ( ] ) } should return false:" + validateBracket("{[(])}"));
+		System.out.println("] { [ ( ] ) } should return false:" + validateBracket("]{[(])}"));
+
+	}
+	
+	public static boolean validateBracket(String str){
+		Stack<Character> openings = new Stack();
+		HashMap<Character, Character> map = new HashMap();
+		map.put('}', '{');
+		map.put(']',  '[');
+		map.put(')', '(');
+		
+		for(int i = 0; i <str.length(); i++){
+			char curr = str.charAt(i);
+			if( openings.isEmpty() && (curr=='}' || curr==']' || curr==')')) {
+				return false;
+			}
+			if( curr!='}' && curr!=']' && curr!=')'){
+				openings.push(curr);
+			}
+			else {
+				if(map.get(curr) != openings.pop().charValue()) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+}
